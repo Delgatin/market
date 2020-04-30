deb8a1a253a305a1864834d1057e85ed03382998
Nino <nino.giovanni.di@gmail.com>
2020-04-30 16:33:18 +0200

    Created a small loop to read messages in with a switch case.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,35 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         new Main();
     }
 
+    boolean programRunning = true;
+    String readMessage = "";
+
+    Scanner scanner = new Scanner(System.in);
+
+
     public Main() {
-        System.out.println("");
-        System.out.println("More setup");
+        while(programRunning) {
+            System.out.println("Please write your message: ");
+            readMessage = scanner.nextLine();
+            handleMessage(readMessage);
+        }
+    }
+
+    private void handleMessage(String message) {
+        switch (message) {
+            case "exit":
+                programRunning = false;
+                System.out.println("System will shutdown.");
+                break;
+            case "continue":
+                System.out.println("Program continues.");
+                break;
+            default:
+                System.out.println("That input was not valid. Please try again.");
+        }
     }
 }
